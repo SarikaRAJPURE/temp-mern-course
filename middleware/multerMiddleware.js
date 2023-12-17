e3c5bed1f9a544ce9958644c7fdd050933d430a9
SarikaRAJPURE <sprajpure@gmail.com>
2023-12-17 08:10:56 -0500

    fix update user

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,5 +1,10 @@
 import multer from "multer";
-//console.log("multer");
+import DataParser from "datauri/parser.js";
+import path from "path";
+
+const storage = multer.memoryStorage();
+
+/* console.log("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // set the directory where uploaded files will be stored
@@ -10,7 +15,15 @@ const storage = multer.diskStorage({
     // set the name of the uploaded file
     cb(null, fileName);
   },
-});
+}); */
 const upload = multer({ storage });
+
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+  console.log(file);
+  const fileExtention = path.extname(file.originalname).toString();
+  return parser.format(fileExtention, file.buffer).content;
+};
 //console.log(upload);
 export default upload;
